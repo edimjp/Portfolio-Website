34b1a23781716434e95910d38a205aba59aa60e2
Edim J Paul <edimjpaul@gmail.com>
2025-04-07 15:22:06 +0100

    added overlays on the index page, remaining more animation and text gradient and mask

--- hamburger.js
+++ hamburger.js
@@ -28,20 +28,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector("nav img.hamburger");
   const sideBar = document.querySelector("aside.side-bar");
   const sideBarCancel = document.querySelector("nav div.cancel span");
+  const sideBarOverlay = document.querySelector("div.sbc-guard");
 
   // Function to open the sidebar
   function openSideBar() {
     sideBar.style.transform = "translateX(0)"; // Slide in
+    sideBarOverlay.style.display = "block";
   }
 
   // Function to close the sidebar
   function closeSideBar() {
     sideBar.style.transform = "translateX(-100%)"; // Slide out
+    sideBarOverlay.style.display = "none";
   }
 
   // Event listeners
   hamburger.addEventListener("click", openSideBar);
   sideBarCancel.addEventListener("click", closeSideBar);
+
+  sideBarCancel.addEventListener("click", closeSideBar);
+
+  sideBarOverlay.addEventListener("click", closeSideBar);
+
+  //Prevent overlay click when inside the sidebar
+
+  // sideBar.addEventListener("click", (e) => {
+  //   e.stopPropagation(); // Prevents click events from propagating to the overlay
+  //   console.log("Inside the side bar");
+  // });
 
   // Swipe gesture detection
   let touchStartX = 0;
